0dc835b8e7e01882c993907096f1dc308aee38f9
Vincent Sanders <vincesanders@hotmail.com>
2020-07-09 15:16:50 -0500

    Added basic scaffolding for Menu component and css reset.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu';
 
 function App() {
   return (
---
+++ src/components/Menu.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+    <header>
+        <div>
+            <a className='logo'>Runespeaker's Edda</a>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a>Menu Item 1</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a>Menu Item 1</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a>Menu Item 1</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a>Menu Item 1</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a>Menu Item 1</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a>Menu Item 1</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a>Menu Item 1</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    );
+}
+
+export default Menu;
