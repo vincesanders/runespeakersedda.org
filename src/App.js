7ff83cb535b977910cf7ba81e1b748888b2e71ff
Vincent Sanders <vincesanders@hotmail.com>
2020-07-09 15:56:20 -0500

    Added CTA component with Odin symbol.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Menu from './components/Menu';
+import CTA from './components/CTA';
 
 function App() {
   return (
     <div>
       <Menu />
+      <CTA />
     </div>
   );
 }
---
+++ src/components/CTA.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import valknut from '../assets/images/Walknut.png';
+
+const CTA = () => {
+    return (
+        <Container>
+            <img src={valknut} />
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    width: 100%;
+    margin-top: 40px;
+    display: flex;
+    justify-content: center;
+    background-color: #050505;
+    padding: 50px;
+    img {
+        width: 45%;
+        box-shadow: 0 0 50px 50px rgba(0, 0, 0, 0.8);
+    }
+`
+
+export default CTA;
